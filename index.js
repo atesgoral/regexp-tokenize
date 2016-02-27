5a4528c60b1376e144454551f5e053e4257849b9
Ates Goral <ates.g@myplanet.com>
2016-02-27 13:39:17 -0500

    Added implementation, started adding tests

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
 
-module.exports = function (re, str) {
+module.exports = function (re, str, responder) {
+  var tokens = re.exec(str);
 
+  if (responder instanceof Function) {
+    if (tokens) {
+      responder.apply(null, tokens);
+    }
+  } else {
+    return new Promise(function (resolve, reject) {
+      if (tokens) {
+        if (responder instanceof Object) {
+          var obj = {};
+
+          for (var p in responder) {
+            obj[p] = tokens[responder[p]];
+          }
+
+          resolve(obj);
+        } else {
+          resolve(tokens);
+        }
+      } else {
+        reject();
+      }
+    });
+  }
 };
--- test/test.js
+++ test/test.js
@@ -1,11 +1,57 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+var expect = chai.expect;
 
 var tokenize = require('../index');
 
 describe('regexp-tokenize', function () {
   it('should be a function', function () {
     expect(tokenize).to.be.a.function;
+  });
+
+  describe('when called with a responder that is a callback function', function () {
+    describe('when there is a match', function () {
+      it('should call the callback with the match results as arguments', function () {
+        var callback = sinon.spy();
+
+        tokenize(/(.+)=(.+)/, 'k=v', callback);
+
+        expect(callback).to.have.been.calledWith('k=v', 'k', 'v');
+      });
+    });
+
+    describe('when there is no match', function () {
+      it('should not call the callback', function () {
+        var callback = sinon.spy();
+
+        tokenize(/(.+)=(.+)/, 'x', callback);
+
+        expect(callback).to.not.have.been.called;
+      });
+    });
+  });
+
+  describe('when called without a responder that is a callback function', function () {
+    describe('when there is a match', function () {
+      describe('when called with a responder that is an object', function () {
+
+      });
+
+      describe('when called without a responder that is an object', function () {
+
+      });
+    });
+
+    describe('when there is no match', function () {
+      it('should return a rejected promise', function () {
+
+      });
+    });
   });
 });
