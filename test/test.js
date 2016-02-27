3bf2fea7185c9182e69a1cb8262836d596e2bb66
Ates Goral <ates.g@myplanet.com>
2016-02-27 14:06:20 -0500

    Added remining tests

--- test/test.js
+++ test/test.js
@@ -3,8 +3,10 @@
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var chaiAsPromised = require("chai-as-promised");
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
 
 var expect = chai.expect;
 
@@ -40,17 +42,31 @@ describe('regexp-tokenize', function () {
   describe('when called without a responder that is a callback function', function () {
     describe('when there is a match', function () {
       describe('when called with a responder that is an object', function () {
+        it('should return a promise that is resolved with the match results mapped to an object by using the responder as a template', function () {
+          var promise = tokenize(/(.+)=(.+)/, 'k=v', { key: 1, value: 2 });
+
+          expect(promise).to.be.an.instanceOf(Promise);
+          expect(promise).to.eventually.deep.equal({ key: 'k', value: 'v' });
+        });
 
       });
 
       describe('when called without a responder that is an object', function () {
+        it('should return a promise that is resolved with the match results', function () {
+          var promise = tokenize(/(.+)=(.+)/, 'k=v');
 
+          expect(promise).to.be.an.instanceOf(Promise);
+          expect(promise).to.eventually.deep.equal([ 'k=v', 'k', 'v' ]);
+        });
       });
     });
 
     describe('when there is no match', function () {
-      it('should return a rejected promise', function () {
+      it('should return a promise that is rejected without any arguments', function () {
+        var promise = tokenize(/(.+)=(.+)/, 'x');
 
+        expect(promise).to.be.an.instanceOf(Promise);
+        expect(promise).to.be.rejectedWith();
       });
     });
   });
